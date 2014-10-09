d8ae6d7808468379674c3429b8f96ada688d62f8
Jason Lim Jia Xheng <jasonlim-93@users.noreply.github.com>
2014-10-09 22:11:11 +0800

    Create wine_search_IT.php
    search page for Pear IT

---
+++ wine_search_IT.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML PUBLIC
+                 "-//W3C//DTD HTML 4.01 Transitional//EN"
+                 "http://www.w3.org/TR/html401/loose.dtd"/>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Jason's Wine Store : Results </title>
+	<style>	
+	input[type='text']
+	{
+	font-family:Lucida Console;
+	}
+	
+	input:focus 
+	{
+    border-color: #0000CC;
+	}
+	
+	td
+	{	
+		font-family:Lucida Console;
+		padding: 12px;
+	}
+	
+	table
+	{
+		align: center;
+		margin: auto;
+		border: 1px ridge #9900CC;;
+		padding: 3px;
+	}
+	</style>
+</head>
+<body>
+	<table width="1200px">
+		<tr>
+			<td align="center">
+				<img src ="wine_logo.jpg" alt="Wine store logo"
+				style="width:450px; height:235px; "/>
+			</td>
+		</tr>
+		
+	</table>
+	
+	</body>
+	</html>
+	
